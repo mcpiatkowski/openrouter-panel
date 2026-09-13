178dc5a2974503733fdef186cc52954843f94b86
Michal <mcpiatkowski@gmail.com>
2026-09-13 20:48:58 +0200

    Add willow answer

---
+++ questions/willow/answer.sh
@@ -0,0 +1,10 @@
+uv run mvp/panel.py \
+    --role answer \
+    --topic willow \
+    --question-file questions/willow/briefing.md \
+    --image images/small/polnoc_3.jpeg \
+    --image images/small/polnoc_4.jpeg \
+    --image images/small/wschod_1.jpeg \
+    --image images/small/kwiatostan_1.jpeg \
+    --image images/small/kwiatostan_2.jpeg \
+    "$@"
